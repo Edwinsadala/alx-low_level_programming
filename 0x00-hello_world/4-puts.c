e4dda77d3c6db7b2b888854a0e664d8de67dcc77
EdwinSadala <eduspartan107@gmail.com>
2023-06-20 04:21:55 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A programme that puts a function
+ * Return:0(sucess)
+ */
+int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle");
+return (0);
+}
+
